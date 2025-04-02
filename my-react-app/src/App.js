5399bb7ac253aee55d7847c744ef9eb8a4c68031
nazar-fomin <nazar2005kiril@gmail.com>
2025-04-02 20:06:03 +0300

    add style

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,21 +1,27 @@
 import { useState } from "react";
-export function ComponentNamber() {
-  const [namber, setnamber] = useState({ namber: 20 });
+import module from "./App.module.css";
+
+export function ComponentNumber() {
+  const [number, setNumber] = useState({ number: 20 });
+
   return (
-    <div>
-      {namber.namber}
-      <button
-        onClick={() =>
-          setnamber({
-            ...namber,
-            namber: namber.namber + 1,
-          })
-        }
-      >
-        Namber
-      </button>
+    <div class={module.container}>
+      <div class={module.contentDiv}>
+        <h1 class={module.text}>{number.number}</h1>
+        <button
+          className={module.btn}
+          onClick={() =>
+            setNumber({
+              ...number,
+              number: number.number + 1,
+            })
+          }
+        >
+          Number
+        </button>
+      </div>
     </div>
   );
 }
 
-export default ComponentNamber;
+export default ComponentNumber;
